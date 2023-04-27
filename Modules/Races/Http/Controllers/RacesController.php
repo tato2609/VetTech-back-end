45eb2a0918a4a0526e75fbc5d3564130a442dfbc
Robinson Horta <hortarobinson@gmail.com>
2023-04-26 22:39:19 -0500

    Refactored RacesController methods to ensure correct handling of response and error messages.
    These changes standardize the use of white-spaces, make corrections to the code's syntax and add error handling and response standards for a more efficient and effective error resolution process.

--- Modules/Races/Http/Controllers/RacesController.php
+++ Modules/Races/Http/Controllers/RacesController.php
@@ -17,19 +17,19 @@ class RacesController extends Controller
     public function index()
     {
         try {
-            $races = Races::where('status',true)->get();
+            $races = Races::where('status', true)->get();
             if ($races) {
-                $response=[
-                    "message"=>"Listado generado con exito",
-                    "data"=>$races
+                $response = [
+                    "message" => "Listado generado con exito",
+                    "data" => $races
                 ];
-            }else {
-                $response=[
-                    "message"=>"No hay razas activas",
-                    "data"=>$races
+            } else {
+                $response = [
+                    "message" => "No hay razas activas",
+                    "data" => $races
                 ];
             }
-            return response()->json($response,200);
+            return response()->json($response, 200);
         } catch (\Throwable $th) {
             throw $th;
         }
@@ -41,25 +41,25 @@ class RacesController extends Controller
      * @param RacesValidateRequest  $request
      * @return Renderable
      */
-    public function store(RacesValidateRequest $request,Races $races)
+    public function store(RacesValidateRequest $request, Races $races)
     {
         try {
-            $veryify = Races::where('name','like', '%' . $request->name . '%')->exist();
+            $veryify = Races::where('name', 'like', '%' . $request->name . '%')->exist();
             if ($veryify) {
-                $response=[
-                    "message"=>"Esta raza ya se encuentra creada",
-                    "data"=>$request->name
+                $response = [
+                    "message" => "Esta raza ya se encuentra creada",
+                    "data" => $request->name
                 ];
-            }else {
+            } else {
                 $races->name          = $request->name;
                 $races->status        = $request->status;
                 $races->save();
-                $response=[
-                    "message"=>"La raza fue creada con exito",
-                    "data"=>$races
+                $response = [
+                    "message" => "La raza fue creada con exito",
+                    "data" => $races
                 ];
             }
-            return response()->json($response,200);
+            return response()->json($response, 200);
         } catch (\Throwable $th) {
             throw $th;
         }
@@ -73,20 +73,10 @@ class RacesController extends Controller
     public function show($id)
     {
         try {
-            if ($id) {
-                $races = Races::find($id);
-                $response= [
-                    "message"=>"Raza encontrada",
-                    "data"=>$races
-                ];
-            }else {
-                $response=[
-                    "message"=>"Error al buscar la raza con el id",
-                    "data"=> $id
-                ];
-            }
-            return response()->json($response,200);
+            $races = Races::findOrFail($id);
+            return response()->json(["message" => "Raza encontrada", "data" => $races], 200);
         } catch (\Throwable $th) {
+            return response()->json(["message" => "Error al buscar la raza con el id", "data" => $id], 400);
             throw $th;
         }
     }
@@ -103,21 +93,21 @@ class RacesController extends Controller
         try {
             $verify = Races::find($request->id);
             if ($verify) {
-                $update = Races::where('id',$request->id)->update([
-                    "name"          =>$request->name,
-                    "status"        =>$request->status
+                $update = Races::where('id', $request->id)->update([
+                    "name"          => $request->name,
+                    "status"        => $request->status
                 ]);
-                $response= [
+                $response = [
                     "message" => "Datos de la raza actualizado con exito",
                     "data"    => $update
                 ];
-            }else {
-                $response= [
+            } else {
+                $response = [
                     "message" => "Error al actualizar los datos de la raza",
                     "data"    => $verify
                 ];
             }
-            return response()->json($response,200);
+            return response()->json($response, 200);
         } catch (\Throwable $th) {
             throw $th;
         }
@@ -132,19 +122,19 @@ class RacesController extends Controller
     {
         $verify = Races::find($id);
         if ($verify) {
-            $destroy=Races::where('id',$id)->update([
-                "status"=> false
+            $destroy = Races::where('id', $id)->update([
+                "status" => false
             ]);
-            $response= [
+            $response = [
                 "message" => "Raza eliminada con exito del sistema",
                 "data"    => $destroy
             ];
-        }else {
-            $response= [
+        } else {
+            $response = [
                 "message" => "Error al eliminar la raza del sistema",
                 "data"    => $verify
             ];
         }
-        return response()->json($response,200);
+        return response()->json($response, 200);
     }
 }
